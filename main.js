b2851b818c0477609f915c3a3ff922cb569e2158
Cody van Haalen <cody.vanhaalen@ru.nl>
2025-05-15 13:29:51 +0200

    added comments to main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron') // import electron modules
+const path = require('path') // built-in node module, not used here actually
 
 const createWindow = () => {
+    // create a new browser window
     const mainWindow = new BrowserWindow({
         width: 1280,
         height: 600,
@@ -9,21 +10,26 @@ const createWindow = () => {
         minHeight: 600,
         backgroundColor: '#f5f7fa',
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
+            nodeIntegration: true, // allow using node in renderer (not very secure but okay for small apps)
+            contextIsolation: false, // allows shared context (again, not super safe but convenient)
         },
-        show: false,
+        show: false, // don't show it immediately
     });
 
+    // load the main HTML file
     mainWindow.loadFile('index.html')
+
+    // only show window when it's ready to avoid flicker
     mainWindow.once('ready-to-show', () => {
         mainWindow.show()
     });
 }
 
+// wait until Electron is fully ready
 app.whenReady().then(() => {
     createWindow()
 
+    // on macOS, re-create a window when clicking the dock icon if none are open
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
@@ -31,6 +37,7 @@ app.whenReady().then(() => {
     })
 })
 
+// quit the app when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
